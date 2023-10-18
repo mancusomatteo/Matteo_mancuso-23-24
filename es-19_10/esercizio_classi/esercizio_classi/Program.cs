e55c16cb9b2c8f4178a44ba6008a2b78c283f3da
matteomancuso <mancuso.matteo.06@itisdalmine.edu.it>
2023-10-18 17:35:56 +0200

    aggiunta esercizio classi per il 19-10

---
+++ es-19_10/esercizio_classi/esercizio_classi/Program.cs
@@ -0,0 +1,45 @@
+﻿using esercizio_classi;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+        Televisore tv1 = new Televisore("Samsung", "Smart TV", 100, 0, 20, true);
+        Televisore tv2 = new Televisore("Sony", "LCD TV", 200, 0, 100, true);
+        Televisore tv3 = new Televisore("Panasonic", "TV LED ULTRA", 18, 0, 80, true);
+        
+        tv1.ON();
+
+        bool acceso1 = tv1.accesa();
+        Console.WriteLine("la tv1 è accesa? " + (acceso1 ?  " si" : "no"));
+        
+
+        tv1.Incrementa_canale();
+        tv1.AlzaVolume(1);
+        Console.WriteLine("informazioni tv1: " +tv1.informazioni());
+
+        tv2.ON();
+
+        bool acceso2 = tv2.accesa();
+        Console.WriteLine("la tv2 è accesa? " + (acceso2 ? " si" : "no"));
+         
+
+        tv2.decrementa_canale();
+        tv2.AbbassaVolume(5);
+        Console.WriteLine("informazioni tv2: " + tv2.informazioni());
+
+
+        tv3.ON();
+
+        bool acceso3 = tv3.accesa();
+        Console.WriteLine("la tv3 è accesa? " + (acceso3 ? " si" : "no"));
+        tv3.Volumeattuale();
+
+        tv3.Canaleattuale();
+
+        Console.WriteLine("informazioni tv3: " + tv3.informazioni());
+
+    }
+}
+
---
+++ es-19_10/esercizio_classi/esercizio_classi/Televisore.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+
+namespace esercizio_classi
+{
+    internal class Televisore
+    {
+        private string marca;
+        private string modello;
+        private int nMaxcanali;
+        private int vMax;
+        private int vMin;
+        private bool aColori;
+        private bool on;
+        private int canaleattuale;
+        private int volumeattuale;
+
+
+        public Televisore(string marca, string modello, bool acolori)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.aColori = aColori;
+            this.nMaxcanali = 100;
+            this.vMax = 50;
+            this.vMin = 0;
+            this.on = false;
+            this.canaleattuale = 1;
+            this.volumeattuale = vMin;
+        }
+        public Televisore(string marca, string modello, int nMaxcanali, int vMax, int vMin, bool aColori)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.nMaxcanali = nMaxcanali;
+            this.vMax = vMax;
+            this.vMin = vMin;
+            this.aColori = aColori;
+            this.on = false;
+            this.canaleattuale = 1;
+            this.volumeattuale = vMin;
+        }
+        public void Marca(string m)
+        {
+            if (string.IsNullOrWhiteSpace(m) == true)
+            {
+                throw new ArgumentException("la marca non può essere vuota");
+
+            }
+            marca = m;
+        }
+        public void Modello(string mo)
+        {
+            if (string.IsNullOrWhiteSpace(mo) == true)
+            {
+                throw new ArgumentException("la marca non può essere vuota");
+
+            }
+            modello = mo;
+        }
+        public void ON()
+        {
+            on = true;
+        }
+        public void alzavolume()
+        {
+            if (on == true)
+            {
+                if (volumeattuale < vMax)
+                {
+                    volumeattuale++;
+                }
+            }
+        }
+
+        public void abbassavolume(int v)
+        {
+            if (on== true)
+            {
+                if (volumeattuale > vMin) {
+                    volumeattuale--;
+                }
+            }
+        }
+        public void AlzaVolume(int n)
+        {
+            if (on == true)
+            {
+                volumeattuale += n;
+                if (volumeattuale < vMax)
+                {
+                    volumeattuale = vMax;
+                }
+                }
+        }
+
+        public void AbbassaVolume(int n)
+        {
+            if (on ==  true)
+            {
+                volumeattuale -= n;
+                if (volumeattuale > vMin)
+                {
+                    volumeattuale = vMin;
+                }
+                }
+        }
+
+        public void Incrementa_canale()
+        
+        {
+            if (on == true)
+            {
+                if (canaleattuale<nMaxcanali)
+                {
+                    canaleattuale++;
+                }
+            }
+        }
+        public void decrementa_canale()
+
+        {
+            if (on)
+            {
+                if (canaleattuale > 1)
+                {
+                    canaleattuale--;
+                }
+            }
+        }
+
+        public bool accesa()
+        {
+            return on;
+        }
+
+        public int Canaleattuale()
+        {
+            return canaleattuale;
+        }
+
+        public int Volumeattuale()
+        {
+            return volumeattuale;
+        }
+
+        public string informazioni()
+        {
+            return (" marca: " + marca + "; modello: " + modello + "; canale: " + canaleattuale + "; volume: " + volumeattuale + "; acceso: " + on );
+        
+        }
+        public void CopiaInfoTelevisione(Televisore televisore)
+        {
+            this.marca = televisore.marca;
+            this.modello = televisore.modello;
+            this.nMaxcanali = televisore.nMaxcanali;
+            this.vMin = televisore.vMin;
+            this.vMax = televisore.vMax;
+            this.aColori = televisore.aColori;
+            this.on = televisore.on;
+            this.canaleattuale = televisore.canaleattuale;
+            this.volumeattuale = televisore.volumeattuale;
+        }
+    }
+    
+
+}
